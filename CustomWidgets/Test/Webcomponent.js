9503ac9c9e04166ca1e5a60a70fe71e5e8c8900b
thomasgeier03 <135979644+thomasgeier03@users.noreply.github.com>
2023-06-08 14:43:29 +0200

    Create Webcomponent.js

---
+++ CustomWidgets/Test/Webcomponent.js
@@ -0,0 +1,25 @@
+(function () {
+    let tmpl = document.createElement('template');
+    tmpl.innerHTML = 
+    `<button type="button" id="myBtn">Helper Button</button>` ; 
+  
+  class PerformanceHelp extends HTMLElement {
+        constructor() {
+            super();
+            this.init();           
+        }
+    
+    init() {            
+              
+            let shadowRoot = this.attachShadow({mode: "open"});
+            shadowRoot.appendChild(tmpl.content.cloneNode(true));
+            this.addEventListener("click", event => {
+            var event = new Event("onClick");
+            this.fireChanged();           
+            this.dispatchEvent(event);
+            });           
+        }
+    
+    customElements.define('custom-button', PerformanceHelp);
+  
+})();
